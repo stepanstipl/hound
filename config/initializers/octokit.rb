ab2f8cf76b98adf23d5a39622bdffca75240b76a
Stepan Stipl <stepan@stipl.net>
2017-02-08 12:43:31 +0000

    feat: Add support for GitHub Enterprise

---
+++ config/initializers/octokit.rb
@@ -0,0 +1,4 @@
+Octokit.configure do |config|
+  config.api_endpoint = Hound::GITHUB_URL + '/api/v3'
+end
+
--- config/initializers/omni_auth.rb
+++ config/initializers/omni_auth.rb
@@ -4,12 +4,17 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   setup = ->(env) do
     options = GithubAuthOptions.new(env)
     env["omniauth.strategy"].options.merge!(options.to_hash)
+    env["omniauth.strategy"].options[:client_options] =  {
+      :site => Hound::GITHUB_URL + '/api/v3',
+      :authorize_url => Hound::GITHUB_URL + '/login/oauth/authorize',
+      :token_url => Hound:: GITHUB_URL + '/login/oauth/access_token'
+    }
   end
 
   provider(
     :github,
     ENV['GITHUB_CLIENT_ID'],
     ENV['GITHUB_CLIENT_SECRET'],
-    setup: setup,
+    setup: setup
   )
 end
