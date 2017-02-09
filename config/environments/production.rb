ac4bb761f2fefebdee2d7f11dc635e967d0fb107
Stepan Stipl <stepan@stipl.net>
2017-02-09 23:54:15 +0000

    fix: fix missing assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,7 +17,7 @@ Houndapp::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  # config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -27,10 +27,10 @@ Houndapp::Application.configure do
 
   # Specifies the header that your server uses for sending files
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
-  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # See everything in the log (default is :info)
   config.log_level = :info
